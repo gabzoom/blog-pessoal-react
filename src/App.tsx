905cdd30d76a2ace583b53917f759497d6cf1eee
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2024-01-18 10:04:30 -0300

    :sparkles: feat: Add os componentes Cadastro e Login

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
 import Home from './pages/home/Home'
 import Cadastro from './pages/cadastro/Cadastro'
+import Login from './pages/login/Login'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
             <Route path="/cadastro" element={<Cadastro />} />
+            <Route path="/login" element={<Login />} />
           </Routes>
         </div>
         <Footer />
