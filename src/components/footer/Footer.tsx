b557ebd05a9b9a83cfa671867b8b5744b8adbb4d
Gabriel Rodrigues <gabriel.rodriguesxs@gmail.com>
2024-01-17 21:31:05 -0300

    :sparkles: feat: Implementa React Router e add rotas /home e /cadastro

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,36 @@
-import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { FacebookLogo, GithubLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
 
 function Footer() {
+
+    let data = new Date().getFullYear()
+
     return (
         <>
             <div className="flex justify-center bg-indigo-900 text-white">
+
                 <div className="container flex flex-col items-center py-4">
-                    <p className='text-xl font-bold'>Blog Pessoal - Generation | Copyright: </p>
+
+                    <p className='text-xl font-bold'>Blog Pessoal - Generation | Copyright: {data}</p>
+
                     <p className='text-lg'>Acesse nossas redes sociais</p>
+
                     <div className='flex gap-2'>
-                        <LinkedinLogo size={48} weight='bold' />
-                        <InstagramLogo size={48} weight='bold' />
-                        <FacebookLogo size={48} weight='bold' />
+                        <a href="https://github.com/gabzoom" target="_blank">
+                            <GithubLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.linkedin.com/in/gabrielrodriguesz" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
+                        </a>
+                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                            <FacebookLogo size={48} weight='bold' />
+                        </a>
                     </div>
+
                 </div>
+
             </div>
         </>
     )
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
 
 function Navbar() {
     return (
         <>
             <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
                 <div className="container flex justify-between text-lg">
-                    <div className='text-2xl font-bold uppercase'>Blog Pessoal</div>
+                    <Link to="/home" className='text-2xl font-bold uppercase'>Blog Pessoal</Link >
 
                     <div className='flex gap-4'>
                         <div className='hover:underline'>Postagens</div>
@@ -14,7 +15,7 @@ function Navbar() {
                         <div className='hover:underline'>Sair</div>
                     </div>
                 </div>
-            </div>
+            </div >
         </>
     )
 }
